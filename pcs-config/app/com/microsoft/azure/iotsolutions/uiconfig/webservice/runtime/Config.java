4a0df1359c0c88922c0c2b173191669bd8c0414a
Devis Lucato <dluc@users.noreply.github.com>
2017-09-03 21:03:02 -0700

    Clean up env vars and config (#20)
    * Remove old scripts
    * Clean up configuration

--- pcs-config/app/com/microsoft/azure/iotsolutions/uiconfig/webservice/runtime/Config.java
+++ pcs-config/app/com/microsoft/azure/iotsolutions/uiconfig/webservice/runtime/Config.java
@@ -11,7 +11,6 @@ public class Config implements IConfig {
 
     private final String NAMESPACE = "com.microsoft.azure.iotsolutions.";
     private final String APPLICATION_KEY = NAMESPACE + "uiconfig.";
-    private final String HOST_NAME_KEY = APPLICATION_KEY + "webservice-hostname";
     private final String PORT_KEY = APPLICATION_KEY + "webservice-port";
     private final String CORS_WHITE_LIST_KEY = APPLICATION_KEY + "cors_whitelist";
     private final String STORAGE_ADAPTER_WEBSERVICE_URL = APPLICATION_KEY + "storageadapter-webservice-url";
@@ -31,16 +30,6 @@ public class Config implements IConfig {
     /// <summary>CORS whitelist, in form { "origins": [], "methods": [], "headers": [] }</summary>
     public String getCorsWhitelist() {
         return this.data.getString(CORS_WHITE_LIST_KEY);
-    }
-
-    /**
-     * Get the hostname where the service listen for requests, e.g. 0.0.0.0 when
-     * listening to all the network adapters.
-     *
-     * @return Hostname or IP address
-     */
-    public String getHostname() {
-        return data.getString(HOST_NAME_KEY);
     }
 
     /// <summary>Service layer configuration</summary>
