59599e10e3547860897bed781075dabcb881f020
Elvin Morales <elmoral@microsoft.com>
2018-10-16 13:58:29 -0700

    Add copyright

--- iothub-manager/test/com/microsoft/azure/iotsolutions/iothubmanager/services/DevicesUnitTest.java
+++ iothub-manager/test/com/microsoft/azure/iotsolutions/iothubmanager/services/DevicesUnitTest.java
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft. All rights reserved.
+
 package com.microsoft.azure.iotsolutions.iothubmanager.services;
 
 import com.microsoft.azure.iotsolutions.iothubmanager.services.exceptions.InvalidInputException;
