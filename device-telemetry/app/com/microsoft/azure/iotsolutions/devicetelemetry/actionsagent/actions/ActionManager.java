9e6ce18d6a0b07f80f3150ca84c129f0ab4ae84c
Hua Zhang <huazhan@microsoft.com>
2018-10-30 09:45:57 -0700

    Add log and error message when no proper action executor available

--- device-telemetry/app/com/microsoft/azure/iotsolutions/devicetelemetry/actionsagent/actions/ActionManager.java
+++ device-telemetry/app/com/microsoft/azure/iotsolutions/devicetelemetry/actionsagent/actions/ActionManager.java
@@ -7,6 +7,7 @@ import com.microsoft.azure.iotsolutions.devicetelemetry.actionsagent.models.AsaA
 import com.microsoft.azure.iotsolutions.devicetelemetry.services.exceptions.ResourceNotFoundException;
 import com.microsoft.azure.iotsolutions.devicetelemetry.services.models.actions.ActionType;
 import com.microsoft.azure.iotsolutions.devicetelemetry.services.runtime.IServicesConfig;
+import play.Logger;
 import play.libs.ws.WSClient;
 
 import java.util.*;
@@ -16,12 +17,12 @@ import java.util.stream.Collectors;
 
 public class ActionManager implements IActionManager {
 
-    private HashMap<ActionType, IActionExecutor> actionExecutors;
+    private HashMap<ActionType, IActionExecutor> actionExecutorsMap;
+    private static final Logger.ALogger log = Logger.of(ActionManager.class);
 
     @Inject
     public ActionManager(IServicesConfig servicesConfig, WSClient wsClient) throws ResourceNotFoundException {
-        this.actionExecutors = new HashMap<ActionType, IActionExecutor>()
-        {{
+        this.actionExecutorsMap = new HashMap<ActionType, IActionExecutor>() {{
             put(ActionType.Email, new EmailActionExecutor(servicesConfig, wsClient));
         }};
     }
@@ -35,9 +36,12 @@ public class ActionManager implements IActionManager {
             .forEach(alarm -> {
                 alarm.getActions().stream()
                     .forEach(action -> {
-                        IActionExecutor executor = this.actionExecutors.get(action.getType());
+                        IActionExecutor executor = this.actionExecutorsMap.get(action.getType());
                         if (executor != null) {
                             tasks.add(executor.execute(action, alarm));
+                        } else {
+                            this.log.error("No IActionExecutor implementation for this action type: "
+                                + action.getType().name());
                         }
                     });
             });
