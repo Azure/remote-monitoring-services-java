acf318f676beccacf5763c4aee34feaf20cf5d95
Hua Zhang <huazhan@microsoft.com>
2018-10-29 17:21:15 -0700

    Refactor ActionManager to support extending multiple executors based on ActionType

--- device-telemetry/app/com/microsoft/azure/iotsolutions/devicetelemetry/actionsagent/actions/ActionManager.java
+++ device-telemetry/app/com/microsoft/azure/iotsolutions/devicetelemetry/actionsagent/actions/ActionManager.java
@@ -6,24 +6,24 @@ import com.google.inject.Inject;
 import com.microsoft.azure.iotsolutions.devicetelemetry.actionsagent.models.AsaAlarmApiModel;
 import com.microsoft.azure.iotsolutions.devicetelemetry.services.exceptions.ResourceNotFoundException;
 import com.microsoft.azure.iotsolutions.devicetelemetry.services.models.actions.ActionType;
-import com.microsoft.azure.iotsolutions.devicetelemetry.services.models.actions.EmailActionServiceModel;
-import com.microsoft.azure.iotsolutions.devicetelemetry.services.models.actions.IActionServiceModel;
 import com.microsoft.azure.iotsolutions.devicetelemetry.services.runtime.IServicesConfig;
 import play.libs.ws.WSClient;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.stream.Collectors;
 
 public class ActionManager implements IActionManager {
 
-    private IActionExecutor actionExecutor;
+    private HashMap<ActionType, IActionExecutor> actionExecutors;
 
     @Inject
     public ActionManager(IServicesConfig servicesConfig, WSClient wsClient) throws ResourceNotFoundException {
-        this.actionExecutor = new EmailActionExecutor(servicesConfig, wsClient);
+        this.actionExecutors = new HashMap<ActionType, IActionExecutor>()
+        {{
+            put(ActionType.Email, new EmailActionExecutor(servicesConfig, wsClient));
+        }};
     }
 
     public CompletionStage executeAsync(List<AsaAlarmApiModel> alarms) {
@@ -34,8 +34,12 @@ public class ActionManager implements IActionManager {
             .collect(Collectors.toList())
             .forEach(alarm -> {
                 alarm.getActions().stream()
-                    .filter(action -> action.getType().equals(ActionType.Email))
-                    .forEach(action -> tasks.add(actionExecutor.execute((EmailActionServiceModel) action, alarm)));
+                    .forEach(action -> {
+                        IActionExecutor executor = this.actionExecutors.get(action.getType());
+                        if (executor != null) {
+                            tasks.add(executor.execute(action, alarm));
+                        }
+                    });
             });
 
         CompletableFuture[] futures = tasks.stream()
